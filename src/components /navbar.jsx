fee322d9a038e670dae4c6320fd51b0c2ffc9a86
1AbhinavKumar <abhinavkumar1105@gmail.com>
2023-10-15 01:29:05 +0530

    commited till 15 oct

---
+++ src/components /navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {FaBars, FaTimes } from "react-icons/fa"
+
+const navbar = () => {
+  return ( 
+    <div>
+        <FaBars />
+    </div>
+  )
+}
+
+export default navbar
